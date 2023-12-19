3bf5979a07e45551097ddf8da8d24fc28112c703
KUSHAL-31 <s.kushal31@gmail.com>
2023-12-19 07:28:58 +0530

    initial commit

---
+++ lib/view/sign in/components/signin_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/view/common%20widgets/back_button.dart';
+
+class SignInBar extends StatelessWidget {
+  const SignInBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        CustomBackButton(),
+        SizedBox(
+          width: 20,
+        ),
+        Text(
+          'Sign in',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        )
+      ],
+    );
+  }
+}
